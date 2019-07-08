556fa45f9d5fa44ec20ec83236f810b193708d63
julian <julianpitney@gmail.com>
2019-07-08 09:24:33 -0400

    Made dlc module more useable

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+
+
+if not os.path.isdir("../videos/"):
+    os.mkdir("../videos/")
+
+if not os.path.isdir("../DLC_PROJECTS/"):
+    is.mkdir("../DLC_PROJECTS")
+
+PROJECTS_FOLDER = "C:/Projects/3D-Kinematics/DLC_PROJECTS"
--- src/dlc.py
+++ src/dlc.py
@@ -1,146 +1,115 @@
 import deeplabcut
+import config
+from tkinter import filedialog
 
-menuMsg = """Select Function: 
-[1] Create Project 
-[2] Extract Frames
-[3] Label Frames 
-[4] Check Labels 
-[5] Create Training Dataset 
-[6] Train Network 
-[7] Evaluate Network 
-[8] Analyze Videos 
-[9] Filter Predictions 
-[10] Create Labeled Videos 
-[11] Plot Trajectories 
-[12] Refine Network 
-[13] Exit
+
+menuMsg = """Select Function:
+[1] Create Project
+[2] Load Project
+[3] Extract Frames
+[4] Label Frames
+[5] Check Labels
+[6] Create Training Dataset
+[7] Train Network
+[8] Evaluate Network
+[9] Analyze Videos
+[10] Filter Predictions
+[11] Create Labeled Videos
+[12] Plot Trajectories
+[13] Extract Outlier Frames
+[14] Refine Labels
+[15] Merge Datasets
+[16] Exit
 Input: """
-
-CONFIG_PATH = ""
-VIDEO_FOLDER_PATH = ""
 
 
 
 
 def create_proj():
 
-    global CONFIG_PATH, VIDEO_FOLDER_PATH
-
-    PROJECT_NAME = input("Project Name: ")
-    EXPERIMENTER_NAME = input("Experimenter Name: ")
-    VIDEO_FOLDER_PATH = input("Video Folder Path: ")
-    VIDEO_PATHS = ['../videos/output1.avi', '../videos/output2.avi']
-    FULL_PATH_WORKING_DIRECTORY = 'C:/Projects/3D-Kinematics/dlc_projects/'
-    CONFIG_PATH = deeplabcut.create_new_project(PROJECT_NAME, EXPERIMENTER_NAME, VIDEO_PATHS, FULL_PATH_WORKING_DIRECTORY, copy_videos=False)
+    projectName = input("Project Name: ")
+    experimenterName = input("Experimenter Name: ")
+    videoFolderPath = input("Video Folder Path: ")
+    videoPaths = ['../videos/output1.avi', '../videos/output2.avi']
+    configPath = deeplabcut.create_new_project(projectName, experimenterName, videoPaths, config.PROJECTS_FOLDER, copy_videos=False)
+    return configPath
 
 
+def load_proj():
 
-def extract_frames():
-
-    global CONFIG_PATH
-
-    EXTRACTION_MODES = ['automatic', 'manual']
-    AUTO_EXTRACTION_METHOD = ['uniform', 'kmeans']
-    FRAME_EXTRACTION_MODE = EXTRACTION_MODES[0]
-    AUTO_EXTRACTION_MODE = AUTO_EXTRACTION_METHOD[0]
-    deeplabcut.extract_frames(CONFIG_PATH, FRAME_EXTRACTION_MODE, AUTO_EXTRACTION_MODE, crop=True)
-
-def label_frames():
-
-    global CONFIG_PATH
-    deeplabcut.label_frames(CONFIG_PATH)
-
-def check_labels():
-
-    global CONFIG_PATH
-    deeplabcut.check_labels(CONFIG_PATH)
-
-def create_training_dataset():
-
-    global CONFIG_PATH
-    deeplabcut.create_training_dataset(CONFIG_PATH, num_shuffles=1)
-
-def train_network():
-
-    global CONFIG_PATH
-    deeplabcut.train_network(CONFIG_PATH, shuffle=1)
-
-def evaluate_network():
-
-    global CONFIG_PATH
-    deeplabcut.evaluate_network(CONFIG_PATH, shuffle=[1], plotting=True)
-
-def analyze_videos():
-
-    global CONFIG_PATH, VIDEO_FOLDER_PATH
-    VIDEO_TYPE = '.avi'
-    deeplabcut.analyze_videos(CONFIG_PATH, VIDEO_FOLDER_PATH, VIDEO_TYPE, save_as_csv=True)
-
-def filter_predictions():
-
-    global CONFIG_PATH
-    VIDEO_PATH = input("Video Path: ")
-    deeplabcut.filterpredictions(CONFIG_PATH, VIDEO_PATH, shuffle=1)
-
-def create_labeled_videos():
-
-    global CONFIG_PATH, VIDEO_FOLDER_PATH
-    deeplabcut.create_labeled_video(CONFIG_PATH, VIDEO_FOLDER_PATH, filtered=True)
-
-def plot_trajectories():
-
-    global CONFIG_PATH, VIDEO_FOLDER_PATH
-    deeplabcut.plot_trajectories(CONFIG_PATH, VIDEO_FOLDER_PATH, filtered=True)
-
-def refine_network():
-
-    global CONFIG_PATH, VIDEO_FOLDER_PATH
-    deeplabcut.extract_outlier_frames(CONFIG_PATH, VIDEO_FOLDER_PATH)
-    deeplabcut.refine_labels(CONFIG_PATH)
-    deeplabcut.merge_datasets(CONFIG_PATH)
-
-
-
-
-
-while(1):
-
-    menuSelection = input(menuMsg)
-    try:
-       menuSelection = int(menuSelection)
-    except ValueError:
-        print("Invalid selection, try again.")
-        continue
-
-    if menuSelection == 1:
-        create_proj()
-    elif menuSelection == 2:
-        extract_frames()
-    elif menuSelection == 3:
-        label_frames()
-    elif menuSelection == 4:
-        check_labels()
-    elif menuSelection == 5:
-        create_training_dataset()
-    elif menuSelection == 6:
-        train_network()
-    elif menuSelection == 7:
-        evaluate_network()
-    elif menuSelection == 8:
-        analyze_videos()
-    elif menuSelection == 9:
-        filter_predictions()
-    elif menuSelection == 10:
-        create_labeled_videos()
-    elif menuSelection == 11:
-        plot_trajectories()
-    elif menuSelection == 12:
-        refine_network()
-    elif menuSelection == 13:
-        exit()
+    projDirectory = filedialog.askdirectory()
+    configPath = projDirectory + "/config.yaml"
+    if os.path.isfile(configPath):
+        return configPath
     else:
-        "Invalid selection, try again."
+        print("Project has no config.yaml file.")
+
+
+main():
+
+    configPath = ""
+
+    while(1):
+
+        menuSelection = input(menuMsg)
+        try:
+           menuSelection = int(menuSelection)
+        except ValueError:
+            print("Invalid selection, try again.")
+            continue
+
+        if menuSelection == 1:
+            configPath = create_proj()
+
+        elif menuSelection == 2:
+            configPath = load_proj()
+
+        elif menuSelection == 3:
+            deeplabcut.extract_frames(configPath, "automatic", "uniform", crop=True)
+
+        elif menuSelection == 4:
+            deeplabcut.label_frames(configPath)
+
+        elif menuSelection == 5:
+            deeplabcut.check_labels(configPath)
+
+        elif menuSelection == 6:
+            deeplabcut.create_training_dataset(configPath, num_shuffles=1)
+
+        elif menuSelection == 7:
+            deeplabcut.train_network(configPath, shuffle=1)
+
+        elif menuSelection == 8:
+            deeplabcut.evaluate_network(configPath, shuffle=[1], plotting=True)
+
+        elif menuSelection == 9:
+        deeplabcut.analyze_videos(configPath, videoFolderPath, videoType=".avi", save_as_csv=True)
+
+        elif menuSelection == 10:
+            deeplabcut.filterpredictions(configPath, videoPath, shuffle=1)
+
+        elif menuSelection == 11:
+            deeplabcut.create_labeled_video(configPath, videoFolderPath, filtered=True)
+
+        elif menuSelection == 12:
+            deeplabcut.plot_trajectories(configPath, videoFolderPath, filtered=True)
+
+        elif menuSelection == 13:
+            deeplabcut.extract_outlier_frames(configPath, videoFolderPath)
+
+        elif menuSelection == 14:
+            deeplabcut.refine_labels(configPath)
+
+        elif menuSelection == 15:
+            deeplabcut.merge_datasets(configPath)
+
+        elif menuSelection == 16:
+            exit()
+
+        else:
+            "Invalid selection, try again."
 
 
 
-
+if __name__ == '__main__':
+	main()
