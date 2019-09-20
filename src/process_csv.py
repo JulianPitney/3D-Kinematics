d76e517d60b9e699e184218e4e5b7436f5193a13
julian <julianpitney@gmail.com>
2019-09-20 13:45:46 -0400

    Started implementing point set registration

--- src/process_csv.py
+++ src/process_csv.py
@@ -1,27 +1,39 @@
 import csv
 import sys
 import os.path
-from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d import axes3d #unused import is actually used by mpl...idk why but don't remove
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-from time import sleep
-import pclpy
+import open3d as o3d
+import copy
 
-csvPath = None
-dirPath = None
+csvPath_1 = None
+csvPath_2 = None
+csvPath_3 = None
+csvPath_4 = None
+
 videoPath = None
+dirPath = None
 
-
-if len(sys.argv) < 2:
-    print("Not enough args...")
+if len(sys.argv) == 5:
+    csvPath_1 = sys.argv[1]
+    csvPath_2 = sys.argv[2]
+    csvPath_3 = sys.argv[3]
+    csvPath_4 = sys.argv[4]
+    dirPath = os.path.dirname(csvPath_1)
+elif len(sys.argv) == 6:
+    csvPath_1 = sys.argv[1]
+    csvPath_2 = sys.argv[2]
+    csvPath_3 = sys.argv[3]
+    csvPath_4 = sys.argv[4]
+    dirPath = os.path.dirname(csvPath_1)
+    videoPath = sys.argv[5]
 else:
-    csvPath = sys.argv[1]
-    dirPath = os.path.dirname(csvPath)
-    videoPath = sys.argv[2]
+    print("Usage: python process_csv.py <csv_path_1> <csv_path_2> <csv_path_3> <csv_path_4> <video_path>")
+    exit()
 
-
-def filter_dlc_3d_csv():
+def filter_dlc_3d_csv(csvPath):
 
     rows = []
     upper_filter_bound = 70
@@ -51,10 +63,10 @@ def filter_dlc_3d_csv():
 
 
 
-# Gets the DLC 3D output csv xyz coordinates and puts them into a list of lists.
+# Gets the DLC 3D output csv xyz coordinates and put them into a list of lists.
 # The indices of the outer list represent frame number, while the inner list index
 # represent 0=x, 1=y and 2=z)
-def get_xyz_vals():
+def get_xyz_vals(csvPath):
 
     frame_vals = []
 
@@ -95,6 +107,8 @@ def get_xyz_vals():
 
     return frame_vals
 
+
+# Plotting is specific to the points being tracked and should be rewritten accordingly.
 def plot_dlc_3d(frame_vals):
 
 
@@ -125,20 +139,21 @@ def plot_dlc_3d(frame_vals):
 
 
 
-def convert_dlc3d_csv_to_pcd(frame_vals):
+def gen_pcd(frame_vals, output_file_name):
 
+
+    # Build ,pcd header
     pcd_header = """# .PCD v.7 - Point Cloud Data file format
 VERSION .7
 FIELDS x y z
 SIZE 8 8 8
 TYPE F F F
 COUNT 1 1 1
-WIDTH 19
-HEIGHT 1
+WIDTH """ + str(len(frame_vals[0][0])) + """\nHEIGHT 1
 VIEWPOINT 0 0 0 1 0 0 0
-POINTS 19
-DATA ascii
-"""
+POINTS """ + str(len(frame_vals[0][0])) + "\nDATA ascii\n"
+
+
 
     # Outer list represents frames, while inner list contains all 19 points for that frame.
     # Each point has an x,y and z value.
@@ -159,21 +174,54 @@ DATA ascii
 
     for frame in frame_points:
 
-        with open('test.pcd', 'w') as pcd_file:
+        with open(output_file_name + '.pcd', 'w') as pcd_file:
 
             pcd_file.write(pcd_header)
 
-            for point in frame:
+
+            for point in frame_points[3000]:
                 pcd_file.write(str(point[0]) + " " + str(point[1]) + " " + str(point[2]) + "\n")
 
         # Each frame gets it's own .pcd file containing all the points for that frame.
-        # We don't want to generate thousands of files so exit after 1 frame.
-        exit()
+        # We don't want to generate thousands of files right now so exit after 1 frame.
+        break
 
 
 
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp])
 
 
-frame_vals = get_xyz_vals()
-#plot_dlc_3d(frame_vals)
-convert_dlc3d_csv_to_pcd(frame_vals)
+
+frame_vals_1 = get_xyz_vals(csvPath_1)
+frame_vals_2 = get_xyz_vals(csvPath_2)
+frame_vals_3 = get_xyz_vals(csvPath_3)
+frame_vals_4 = get_xyz_vals(csvPath_4)
+
+gen_pcd(frame_vals_1, "1")
+gen_pcd(frame_vals_2, "2")
+gen_pcd(frame_vals_3, "3")
+gen_pcd(frame_vals_4, "4")
+
+source = o3d.io.read_point_cloud('1.pcd')
+target = o3d.io.read_point_cloud('2.pcd')
+threshold = 0.02
+
+# perform initial alignment
+trans_init = np.asarray([[0.862, 0.011, -0.507, 0.5],
+                         [-0.139, 0.967, -0.215, 0.7],
+                         [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
+draw_registration_result(source, target, trans_init)
+
+
+# ICP Registration
+reg_p2p = o3d.registration.registration_icp(
+    source, target, threshold, trans_init,
+    o3d.registration.TransformationEstimationPointToPoint())
+draw_registration_result(source, target, reg_p2p.transformation)
+
