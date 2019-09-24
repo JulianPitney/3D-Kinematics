823c7cf6fe6a6e9ab553d38a3f25291f25489f2f
julian <julianpitney@Gmail.com>
2019-09-24 15:39:03 -0400

    Got point-cloud pair-wise registration working correctly

--- src/process_csv.py
+++ src/process_csv.py
@@ -7,6 +7,8 @@ import numpy as np
 import cv2
 import open3d as o3d
 import copy
+from open3d.open3d.geometry import estimate_normals
+
 
 csvPath_1 = None
 csvPath_2 = None
@@ -188,6 +190,70 @@ POINTS """ + str(len(frame_vals[0][0])) + "\nDATA ascii\n"
 
 
 
+
+
+
+def prepare_dataset(voxel_size, source, target):
+    print(":: Load two point clouds and disturb initial pose.")
+    trans_init = np.asarray([[0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0],
+                             [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
+    source.transform(trans_init)
+    draw_registration_result(source, target, np.identity(4))
+
+
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+
+
+def preprocess_point_cloud(pcd, voxel_size):
+
+    # TODO: Failing here
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    estimate_normals(pcd, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.registration.compute_fpfh_feature(
+        pcd,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd, pcd_fpfh
+
+
+
+def execute_global_registration(source_down, target_down, source_fpfh,
+                                target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+    result = o3d.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh, distance_threshold,
+        o3d.registration.TransformationEstimationPointToPoint(False), 4, [
+            o3d.registration.CorrespondenceCheckerBasedOnEdgeLength(0.9),
+            o3d.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.registration.RANSACConvergenceCriteria(4000000, 500))
+    return result
+
+
+
+def refine_registration(source, target, source_fpfh, target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 0.4
+    print(":: Point-to-plane ICP registration is applied on original point")
+    print("   clouds to refine the alignment. This time we use a strict")
+    print("   distance threshold %.3f." % distance_threshold)
+    result = o3d.registration.registration_icp(
+        source, target, distance_threshold, result_ransac.transformation,
+        o3d.registration.TransformationEstimationPointToPoint())
+    return result
+
+
+
+
+
 def draw_registration_result(source, target, transformation):
     source_temp = copy.deepcopy(source)
     target_temp = copy.deepcopy(target)
@@ -208,20 +274,21 @@ gen_pcd(frame_vals_2, "2")
 gen_pcd(frame_vals_3, "3")
 gen_pcd(frame_vals_4, "4")
 
-source = o3d.io.read_point_cloud('1.pcd')
-target = o3d.io.read_point_cloud('2.pcd')
-threshold = 0.02
 
-# perform initial alignment
-trans_init = np.asarray([[0.862, 0.011, -0.507, 0.5],
-                         [-0.139, 0.967, -0.215, 0.7],
-                         [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
-draw_registration_result(source, target, trans_init)
+voxel_size = 0.4
+source, target, source_down, target_down, source_fpfh, target_fpfh = \
+    prepare_dataset(voxel_size)
+
+result_ransac = execute_global_registration(source_down, target_down,
+                                                source_fpfh, target_fpfh,
+                                                voxel_size)
+print(result_ransac)
+draw_registration_result(source_down, target_down, result_ransac.transformation)
+
+result_icp = refine_registration(source, target, source_fpfh, target_fpfh,
+                                 voxel_size)
+print(result_icp)
+draw_registration_result(source, target, result_icp.transformation)
 
 
-# ICP Registration
-reg_p2p = o3d.registration.registration_icp(
-    source, target, threshold, trans_init,
-    o3d.registration.TransformationEstimationPointToPoint())
-draw_registration_result(source, target, reg_p2p.transformation)
 
